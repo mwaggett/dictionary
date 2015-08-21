a465dc97944d89f80c682704fe046f80d8534fe6
mwaggett <mwaggett@alumni.reed.edu>
2015-08-21 12:36:49 -0700

    Customize files for project.

--- src/test/java/ClearRule.java
+++ src/test/java/ClearRule.java
@@ -5,7 +5,7 @@ public class ClearRule extends ExternalResource {
   protected void before() { }
 
   protected void after() {
-    Class.clear(); //change to whatever class names created
-    // any other classes in app
+    Definition.clear();
+    Word.clear();
   }
 }
