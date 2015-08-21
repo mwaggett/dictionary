b0ecc02fc977b0c9c9db36bda5de6e5aa451d3be
mwaggett <mwaggett@alumni.reed.edu>
2015-08-21 11:30:51 -0700

    Add initial files.

---
+++ src/main/java/Class.java
@@ -0,0 +1,13 @@
+// import stuff
+
+public class Class {
+
+  private <Class> mMemberVariable();
+
+  public Class (<Class> argument) {
+    // instantiate - assign values to variables, etc.
+  }
+
+  // Class methods
+
+}
---
+++ src/test/java/ClassTest.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+// import anything else needed
+
+public class ClassTest {
+
+  // ~~UNIT TESTING~~
+  // @Test
+  // public void methodName_whatIsBeingTested_desiredResult() {
+  //   App a = new App();
+  //   <Class> expValue = x;
+  //   assertEquals(expValue, a.methodName(param));
+  // }
+
+}
