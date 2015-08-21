efcc9d5fd8b252a98bdb3805f15c58448536cc45
mwaggett <mwaggett@alumni.reed.edu>
2015-08-21 13:49:29 -0700

    Display list of words on homepage, along with link to add a new word. Display new word form that, when submitted, routes back to homepage with new word added to list. Integration tests pass.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,26 +11,45 @@ import java.util.Map;
 public class App {
 
   public static void main(String[] args) {
-    // staticFileLocation("/public"); // Relative path for images, css, etc.
-    // String layout = "templates/layout.vtl";
-    //
-    // get("/", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/index.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/newpage", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/newpage.vtl");
-    //
-    //   <Class> param = request.queryParams("param");
-    //   model.put("param", param);
-    //   <Class> result = someMethod(param);
-    //   model.put("result", result);
-    //
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    staticFileLocation("/public"); // Relative path for images, css, etc.
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      ArrayList<Word> words = request.session().attribute("words");
+      if (words == null) {
+        words = new ArrayList<Word>();
+        request.session().attribute("words", words);
+      }
+      model.put("words", words);
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/words/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/new-word-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      ArrayList<Word> words = request.session().attribute("words");
+      if (words == null) {
+        words = new ArrayList<Word>();
+        request.session().attribute("words", words);
+      }
+
+      Word newWord = new Word(request.queryParams("newWord"));
+      words.add(newWord);
+      model.put("words", words);
+
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,12 +5,15 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
 
   public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
   public WebDriver getDefaultDriver() {
       return webDriver;
   }
@@ -18,19 +21,28 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // ~~INTEGRATION TESTING~~
-  // @Test
-  // public void rootTest() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("Something");
-  // }
-  //
-  // @Test
-  // public void newPage_desiredResult() {
-  //   goTo("starting_page_url");
-  //   fill("#input_id").with("input");
-  //   submit(".btn");
-  //   assertThat(pageSource()).contains("Some result of input");
-  // }
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Dictionary!");
+  }
+
+  @Test
+  public void homepage_navigatesToNewWordForm() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Add a New Word"));
+    assertThat(pageSource()).contains("What is your word?");
+  }
+
+  @Test
+  public void newWordForm_addsWordToHomepage() {
+    goTo("http://localhost:4567/");
+    click("a", withText("Add a New Word"));
+    fill("#newWord").with("house");
+    submit(".btn");
+    assertThat(pageSource()).contains("house");
+  }
+  // ^This doesn't work if I say goTo("http://localhost:4567/words/new");
+  // Why is that?
 
 }
